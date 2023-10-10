7383fd9ec4bd62b9b3811ef2c6a803953c78888b
Kamila Shagimardanova <114108569+kamilories@users.noreply.github.com>
2023-10-10 12:54:52 +0300

    добавление возможности редактирования

--- local/templates/main/components/bitrix/news.list/my_slider/template.php
+++ local/templates/main/components/bitrix/news.list/my_slider/template.php
@@ -17,8 +17,12 @@ $this->setFrameMode(true);
     <div class="col-lg-12">
         <div class="product_img_slide owl-carousel owl-theme">
             <?php foreach ($arResult['ITEMS'] as $arItem): ?>
+                <?php
+                $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                ?>
                 <?php if (!empty($arItem['PREVIEW_PICTURE']['SRC'])): ?>
-                    <div>
+                    <div id="<?=$this->GetEditAreaId($arItem['ID']);?>">
                         <a href="<?=$arItem['PROPERTIES']['link']['VALUE']?>"><img src="<?=$arItem['PREVIEW_PICTURE']['SRC']; ?>" alt="#" ></a>
                         <h1 align="center"><?=$arItem['NAME']?></h1>
                         <p align="center"><?=$arItem['PREVIEW_TEXT']?></p>
